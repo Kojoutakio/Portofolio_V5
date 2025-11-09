53a32859b3e78e39dc0aa3864a1cd8b8aecf2180
Kojouta <earthkun03@gmail.com>
2025-11-09 07:34:41 +0700

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -80,9 +80,9 @@ const PAUSE_DURATION = 2000;
 const WORDS = ["Network & Telecom Student", "Tech Enthusiast"];
 const TECH_STACK = ["React", "Javascript", "Node.js", "Tailwind"];
 const SOCIAL_LINKS = [
-  { icon: Github, link: "https://github.com/EkiZR" },
+  { icon: Github, link: "https://github.com/Kojoutakio" },
   { icon: Linkedin, link: "https://www.linkedin.com/in/ekizr/" },
-  { icon: Instagram, link: "https://www.instagram.com/ekizr_/?hl=id" }
+  { icon: Instagram, link: "https://www.instagram.com/kojouta/" }
 ];
 
 const Home = () => {
